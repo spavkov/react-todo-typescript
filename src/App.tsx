38041d65cf78a3dfa3e759a9019e5c3d0a9070f5
spavkov <spavkov@gmail.com>
2019-09-19 17:21:00 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+          hello typescript
+    </div>
+  );
+}
+
+export default App;
